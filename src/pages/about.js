994ba376c07cdd28aff7bdaba6072a1acee65e3c
Ed Mangini <me@emangini.com>
2022-07-01 09:08:44 -0400

    Updated About Again.

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,31 +8,49 @@ export default function AboutPage() {
 
 
       <h2>whoami</h2>
+      <h3>I'm a technologist</h3>
       <p>
-        I'm both a software technologist and a people leader.
+        I enjoy strategic discussions that construct and refine technical vision.
 
-        I enjoy strategic discussions that construct and refine vision and I have an unquenchable thirst for the
-        tactical elements of delivery.
-
-        I love coaching and mentoring, whether it be fresh-out individual contributors or other leaders looking towards
-        the next leg of their journey.
+        I have an unquenchable thirst for the tactical elements of delivery. Whether we call it engineering excellence
+        or the path to sensible defaults, finding ways to optimize the time and effort spent delivering solutions to
+        our constituents is something I enjoy spending time on.
 
         I'm often described as very "meta". I love to be an "architect of architects" or a "leader of leaders".
       </p>
 
-      <p>>
-        I try hard to keep my hands from wandering too far from the keyboard. I'm not sure I'm completely tech agnostic,
-        but I definitely lean towards polyglot. The characteristics of different languages have pros and cons that constitute
-        tradeoffs in architectural decisions just as any other technology.
-
-        I've whet my appetite over the years delivering projects with (in no particular order):
-          Java, Kotlin, Scala, C, C++, Python, Ruby, PHP, Perl, Erlang, Go, Swift, JavaScript and C#.
-
+      <h3>I design things...</h3>
+      <p>
         I have a passion for distributed systems, problems of scale and the underlying technologies that comprise these
         scenarios. Holistically, I consider myself a generalist striving for technological understanding both a
         mile wide and a mile deep. While I know tradeoffs of time and urgency prevent this from being possible, the
         goal keeps me nimble.
 
+        I consider myself a perpetual student of patterns and approaches.
+      </p>
+
+      <h3>I still code...</h3>
+      <p>
+        I try hard to keep my hands from wandering too far from the keyboard. I'm not sure I'm completely tech agnostic,
+        but I definitely lean towards polyglot. The characteristics of different languages have pros and cons that constitute
+        tradeoffs in architectural decisions just as any other technology.
+
+        I've delivered projects over the years in a number of languages and various tech stacks.
+      </p>
+
+
+      <h3>I'm a people leader...</h3>
+      <p>
+        I love coaching and mentoring, whether it be fresh-out individual contributors or other leaders looking towards
+        the next leg of their journey.
+
+        I believe in generative culture, ally-ship, bottoms-up, and servant style leadership. Software, technology and
+        innovation are all group efforts. My goal is to present a bias towards teamwork.
+      </p>
+
+
+      <h3>I'm always curious...</h3>
+      <p>
         I still have that insatiable curiosity of a 4 year old asking "why"? I believe that innovation, progress and
         quality comes from a culture built on asking questions rather than "having" answers.
       </p>
